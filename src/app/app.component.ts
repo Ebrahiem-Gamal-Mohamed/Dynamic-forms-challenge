db41892d4f3c12bdea2f9243ddd948dcd4be61d7
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2021-01-02 17:14:59 +0200

    modify style, app modules imports and app component logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { debounceTime, switchMap, distinctUntilChanged, tap } from 'rxjs/operators';
+import { BaseControl } from './control-types/base-control.model';
+import { FormDataControlService } from './_services/form-data-control.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dynamic-forms-challenge';
+  textControl: FormControl;
+  formDataControls$: Observable<BaseControl<any>[]>;
+
+  constructor(private fdc: FormDataControlService) {
+    this.textControl = new FormControl('');
+  }
+
+  ngOnInit(): void {
+    this.formDataControls$ = this.textControl.valueChanges
+      .pipe(
+        debounceTime(600),
+        distinctUntilChanged(),
+        switchMap((res: string) => {
+          return this.fdc.getFormData(res);
+        }),
+        tap(data => console.log(data))
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormControlComponent } from './dynamic-form-control/dynamic-form-control.component';
+import { FormDataControlService } from './_services/form-data-control.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormControlComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [FormDataControlService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
