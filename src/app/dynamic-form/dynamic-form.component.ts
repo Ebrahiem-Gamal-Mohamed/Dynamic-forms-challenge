0dc6d999c3e3485907f73859bb87fe67d23587b7
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2021-01-02 17:13:03 +0200

    add dynamic form wrapper compoent and implement its logic.

---
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BaseControl } from '../control-types/base-control.model';
+
+import { FormDataControlService } from '../_services/form-data-control.service';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.scss'],
+})
+export class DynamicFormComponent implements OnInit, OnChanges {
+  @Input() formDataControls: BaseControl<string>[] = [];
+  formData: FormGroup;
+  payLoad = '';
+
+  constructor(private fdc: FormDataControlService) {}
+
+  ngOnChanges(data: SimpleChanges): void {
+    this.payLoad = '';
+    if (data['formDataControls'] && data['formDataControls'].currentValue) {
+      this.formDataControls = data['formDataControls'].currentValue as any;
+      this.formData = this.fdc.toFormGroup(this.formDataControls);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() { // check form validation..
+    if (this.formData && this.formData.valid) {
+      this.payLoad = JSON.stringify(this.formData.getRawValue());
+    }
+  }
+
+  resetForm() { // reset form, formControls and payload after cancelling
+    this.formData.reset();
+    this.formDataControls = [];
+    this.payLoad = '';
+  }
+}
